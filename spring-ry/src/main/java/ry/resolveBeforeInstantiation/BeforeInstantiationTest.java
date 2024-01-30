02629182ef751b425b968e1507dd9083f2ef755d
duidui <zhiyongxu>
2024-01-30 14:16:22 +0800

    InstantiationAwareBeanPostProcessor

---
+++ spring-ry/src/main/java/ry/resolveBeforeInstantiation/BeforeInstantiationTest.java
@@ -0,0 +1,17 @@
+package ry.resolveBeforeInstantiation;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author: xzy
+ * @date: 2024/1/30
+ */
+public class BeforeInstantiationTest {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "resolve.before.instantiation/beans.xml");
+        BeforeInstantiationUser user = context.getBean(BeforeInstantiationUser.class);
+        System.out.println(user);
+        context.registerShutdownHook();
+    }
+}
